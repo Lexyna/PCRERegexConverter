b841033c14534fab5f763ef434f31c972e4c25d5
Lexyna <lexieverena@outlook.de>
2023-04-03 19:18:05 +0200

    Added more tests for AFA

--- test/AFATests.cs
+++ test/AFATests.cs
@@ -51,4 +51,54 @@ public class AFATests
 
     }
 
+    [Fact]
+    public void TwoLookaheads()
+    {
+
+        string regex = "(?=b)a(?=a)";
+
+        Lexer lexer = new Lexer(regex);
+        lexer.Tokenize();
+        lexer.GetTokens().ForEach(t =>
+        {
+            if (t.tokenOP != Token.OP.Class) return;
+            ((ClassToken)t).ConvertToGroup();
+        });
+
+        ParserSimplifier parser = new ParserSimplifier(lexer.GetTokens());
+        parser.Simplify();
+
+        Automaton automaton = new Automaton(parser.GetTokens());
+        automaton.SetStateName();
+
+        EpsilonEliminator.RemoveEpsilonFromState(automaton.startStates[0]);
+        Assert.Equal(2, automaton.GetUniversalTransitionCount());
+
+    }
+
+    [Fact]
+    public void ThreeLookaheads()
+    {
+
+        string regex = "(?=b)a(?=a)(?=ac*)b";
+
+        Lexer lexer = new Lexer(regex);
+        lexer.Tokenize();
+        lexer.GetTokens().ForEach(t =>
+        {
+            if (t.tokenOP != Token.OP.Class) return;
+            ((ClassToken)t).ConvertToGroup();
+        });
+
+        ParserSimplifier parser = new ParserSimplifier(lexer.GetTokens());
+        parser.Simplify();
+
+        Automaton automaton = new Automaton(parser.GetTokens());
+        automaton.SetStateName();
+
+        EpsilonEliminator.RemoveEpsilonFromState(automaton.startStates[0]);
+        Assert.Equal(3, automaton.GetUniversalTransitionCount());
+
+    }
+
 }
