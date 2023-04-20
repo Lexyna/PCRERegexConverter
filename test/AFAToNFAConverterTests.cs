7b517630910ff16ae298af30d8d51925edc94126
Thubeauville <thubeauv@goldbeck.biz>
2023-04-20 11:13:43 +0200

    Added more tests for afa conversion

--- test/AFAToNFAConverterTests.cs
+++ test/AFAToNFAConverterTests.cs
@@ -199,4 +199,105 @@ public class AFAToNFAConverterTests
         Assert.False(converter.nfa.AcceptsWord("ae"));
         Assert.False(converter.nfa.AcceptsWord("aae"));
     }
+
+    [Fact]
+    public void ConvertComplexAfa()
+    {
+
+        string regex = "(?=a)a*(?=ef*)ef";
+
+        Lexer lexer = new Lexer(regex);
+        lexer.Tokenize();
+        lexer.GetTokens().ForEach(t =>
+        {
+            if (t.tokenOP != Token.OP.Class) return;
+            ((ClassToken)t).ConvertToGroup();
+        });
+
+        ParserSimplifier parser = new ParserSimplifier(lexer.GetTokens());
+        parser.Simplify();
+
+        Automaton automaton = new Automaton(parser.GetTokens());
+        automaton.SetStateName();
+
+        EpsilonEliminator.RemoveEpsilonFromState(automaton.startStates[0]);
+
+        AFAToNFAConverter converter = new AFAToNFAConverter(automaton);
+
+        Assert.False(converter.nfa.AcceptsWord("a"));
+        Assert.False(converter.nfa.AcceptsWord("ae"));
+
+        Assert.True(converter.nfa.AcceptsWord("aaaaaef"));
+        Assert.True(converter.nfa.AcceptsWord("aaef"));
+    }
+
+    [Fact]
+    public void ConvertComplexAfa2()
+    {
+
+        string regex = "(?=a)a*(f|c*g)|gh+r";
+
+        Lexer lexer = new Lexer(regex);
+        lexer.Tokenize();
+        lexer.GetTokens().ForEach(t =>
+        {
+            if (t.tokenOP != Token.OP.Class) return;
+            ((ClassToken)t).ConvertToGroup();
+        });
+
+        ParserSimplifier parser = new ParserSimplifier(lexer.GetTokens());
+        parser.Simplify();
+
+        Automaton automaton = new Automaton(parser.GetTokens());
+        automaton.SetStateName();
+
+        EpsilonEliminator.RemoveEpsilonFromState(automaton.startStates[0]);
+
+        AFAToNFAConverter converter = new AFAToNFAConverter(automaton);
+
+        Assert.False(converter.nfa.AcceptsWord("a"));
+        Assert.False(converter.nfa.AcceptsWord("acc"));
+
+        Assert.True(converter.nfa.AcceptsWord("af"));
+        Assert.True(converter.nfa.AcceptsWord("aaaaaf"));
+        Assert.True(converter.nfa.AcceptsWord("acg"));
+        Assert.True(converter.nfa.AcceptsWord("aacg"));
+        Assert.True(converter.nfa.AcceptsWord("accg"));
+        Assert.True(converter.nfa.AcceptsWord("aaacccg"));
+        Assert.True(converter.nfa.AcceptsWord("ghr"));
+        Assert.True(converter.nfa.AcceptsWord("ghhhr"));
+
+    }
+
+    [Fact]
+    public void NonAcceptingAFAWithMultipleLookaheads()
+    {
+
+        string regex = "(?=a)a*(?=d)e";
+
+        Lexer lexer = new Lexer(regex);
+        lexer.Tokenize();
+        lexer.GetTokens().ForEach(t =>
+        {
+            if (t.tokenOP != Token.OP.Class) return;
+            ((ClassToken)t).ConvertToGroup();
+        });
+
+        ParserSimplifier parser = new ParserSimplifier(lexer.GetTokens());
+        parser.Simplify();
+
+        Automaton automaton = new Automaton(parser.GetTokens());
+        automaton.SetStateName();
+
+        EpsilonEliminator.RemoveEpsilonFromState(automaton.startStates[0]);
+
+        AFAToNFAConverter converter = new AFAToNFAConverter(automaton);
+
+        Assert.False(converter.nfa.AcceptsWord("e"));
+        Assert.False(converter.nfa.AcceptsWord("a"));
+        Assert.False(converter.nfa.AcceptsWord("ae"));
+        Assert.False(converter.nfa.AcceptsWord("aee"));
+        Assert.False(converter.nfa.AcceptsWord("aaee"));
+
+    }
 }
